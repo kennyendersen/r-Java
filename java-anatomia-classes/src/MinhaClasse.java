235d788fdb145cb6683543a0ef609791ada69102
kenny <kennyfreitas00@gmail.com>
2025-02-08 23:46:58 -0400

    commit 2

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,9 +1,19 @@
 public class MinhaClasse{
 
 public static void main(String [] args){
+   
+    String primeiroNome = "Kenny";
+    String segundoNome = "Endersen";
 
-    System.out.print("Hello, World?");
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
 
+    System.out.print(nomeCompleto);
 }
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+
+    return "Resultado do método = " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
 
 }
