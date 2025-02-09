da829d9be34b01ff8070c1de55109667b8e03ae9
kenny <kennyfreitas00@gmail.com>
2025-02-09 00:24:12 -0400

    variaveis

--- java-anatomia-classes/src/edu/kenny/primeirasemana/BoletimEstudantil.java
+++ java-anatomia-classes/src/edu/kenny/primeirasemana/BoletimEstudantil.java
@@ -1,7 +1,9 @@
+package edu.kenny.primeirasemana;
+
 public class BoletimEstudantil {
     
     public static void main(String [] args){
-        int mediaFinal = 3;
+        int mediaFinal = 8;
             if (mediaFinal < 6 )
                 System.out.println("Reprovado");
             else if (mediaFinal == 6)
