d1d166b17028ab55e8776c499326e60556551e7a
kenny <kennyfreitas00@gmail.com>
2025-02-10 17:40:05 -0400

    up

---
+++ java-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,10 @@
+public class TiposVariaveis {
+    
+    public static void main (String [] args){
+        System.out.print("Hello, World");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        
+        final double VALOR_DE_PI = 3.14;
+    }
+}
