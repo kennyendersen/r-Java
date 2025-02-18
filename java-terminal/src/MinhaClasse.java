c78640f2f3dcf19a1f105b3b2dd1ea4770d5130c
kenny <kennyfreitas00@gmail.com>
2025-02-18 17:38:34 -0400

    classe

---
+++ java-terminal/src/MinhaClasse.java
@@ -0,0 +1,6 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("\033[1;34mOi, fui executado pelo terminal\033[m");
+        System.out.println("\033[1;34mMy name is Kenny Endersen\033[m");
+    }
+}
